a26303d1090ae885f232d870d56328302d44c064
ajones-stl <65673112+ajones-stl@users.noreply.github.com>
2023-06-15 10:49:55 -0500

    Update streamlit_app.py
    response formating

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#normalizes the responmse
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# create a table
+streamlit.dataframe(fruityvice_normalized)

0144b8d1a9c2b04246d03401ffad38cdc0b2d2fd
ajones-stl <65673112+ajones-stl@users.noreply.github.com>
2023-06-16 07:37:28 -0500

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # create a table
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)

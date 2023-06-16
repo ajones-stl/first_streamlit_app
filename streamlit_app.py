35dc77ebce88bae4bd37696f57cf9c44ac59effb
ajones-stl <65673112+ajones-stl@users.noreply.github.com>
2023-06-16 10:51:09 -0500

    Update streamlit_app.py
    FORMAT FIX

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
 else
@@ -36,6 +36,7 @@ else
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # create a table
   streamlit.dataframe(fruityvice_normalized)
+  
 except URLError as e:
 streamlit.error()
 
